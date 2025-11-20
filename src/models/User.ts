97c4b0a18a6dbf30048a023bc03dde04b07ee019
Kevin <kevinadikasaputragithub@gmail.com>
2025-11-20 22:53:52 +0700

    :wrench: Chore: structure data account

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  password: string;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.models.User ||
+  mongoose.model<IUser>('User', UserSchema);
